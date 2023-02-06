af5e09fd469adffec2d56a42c7d3c16dfc0fc8a3
MarkDennis1 <markdennissanchez1@gmail.com>
2023-02-06 19:22:27 +0800

    separate button to components

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -8,14 +8,9 @@
             <div class="flex flex-col flex-grow md:flex-row justify-between gap-4">
                 <!-- buttons -->
                 <a href="{{route('movies.index')}}">
-                    <div class="relative">
-                        <div class="inline-block cursor-pointer gap-2 px-2 py-1 md:px-4 md:py-2 text-white ring-1 ring-inset ring-[#F33A2E] rounded-lg">
-                            Reset
-                        </div>
-                        <div class="flex absolute top-0 left-0 cursor-pointer hover:-top-1 hover:-left-1.5 duration-150 gap-2 px-2 py-1 md:px-4 md:py-2 rounded-lg bg-white ring-1 ring-[#F33A2E] text-[#F33A2E] font-bold">
-                            Reset
-                        </div>
-                    </div>
+                    <x-secondary-button>
+                        Reset
+                    </x-secondary-button>
                 </a>
 
                 <!-- search -->
@@ -35,20 +30,9 @@
 
             <!-- Add button -->
             <a href="{{route('movies.create')}}">
-                <div class="relative">
-                    <div class="hidden md:flex cursor-pointer gap-2 px-4 py-2 text-white ring-1 ring-inset ring-[#F33A2E] rounded-lg">
-                        Add
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" /></svg>
-                    </div>
-                    <div class="hidden md:flex absolute top-0 left-0 cursor-pointer hover:-top-1 hover:-left-1 duration-150 gap-2 px-4 py-2 rounded-lg bg-[#F33A2E] text-white font-bold">
-                        Add
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" /></svg>
-                    </div>
-                </div>
-
-                <div class="md:hidden z-10 flex justify-center items-center fixed bottom-8 right-4 cursor-pointer gap-2 px-4 py-2 text-white border rounded-full bg-[#F33A2E] hover:bg-[#A2261E] text-2xl">
-                    +
-                </div>
+                <x-primary-button>
+                    Add
+                </x-primary-button>
             </a>
         </div>
         @if (count($movies) === 0)
